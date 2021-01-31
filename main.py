1f2e7000d498a335250a115a0adaa7890b747b15
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2021-01-31 11:24:21 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Image classification
+
+#Import libraries
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras import layers
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
